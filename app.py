6a60c4a6b45522ca82f94b536a3f09dcb36bd614
hoyoul_lee <hoyoul321@gmail.com>
2021-04-22 21:05:34 +0900

    flask_sample

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
-@app.rount ('/')
-def index():
-    return 'Hello World'
-if __name__ = '__main__':
-    app.run(debug = True)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+if __name__ =="__main__":
+    app.run()
