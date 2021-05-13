098954a47852a2ba37d84864048f51a8dd38d99c
hoyoul_lee <hoyoul321@gmail.com>
2021-05-13 18:58:53 +0900

    --mysql conn

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
-'''
-import os
-from flask import Flask
 
+from flask import Flask, render_template
+
+#Flask 객체 인스턴스 생성
 app = Flask(__name__)
-@app.route('/', methods = ['GET'])
-def hello():
-    return 'Hello, World!'
-if __name__ =="__main__":
-    app.run()
-'''
-from website import create_app
-from waitress import serve
-from simplejson import dumps
-from website.config import ProductionConfig, DebugConfig
-from sys import exit
-from werkzeug.middleware.proxy_fix import ProxyFix
 
-# Do NOT set BIND_HOST and BIND_PORT if using Docker
-# will default to 0.0.0.0:80 and get exposed
+@app.route('/') # 접속하는 url
+def index():
+    from utils import mysql
+    res =  mysql.mysql_sample_query()
+    print(res)
+    return render_template('index.html', data =res )
 
-app = create_app()
-proxied = app
-if app.config['PROXIED']:
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
+if __name__=="__main__":
+    app.run(debug=True)
 
-if __name__ == "__main__":
-    if app.config['DEBUG']:
-        app.run(host=app.config['BIND_HOST'], port=app.config['BIND_PORT'])
-    else:
-        serve(app, host=app.config['BIND_HOST'], port=app.config['BIND_PORT'])
---
+++ utils/mysql.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import mysql.connector
+from mysql.connector import Error
+
+def mysql_sample_query():
+    try: 
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1111",
+            database='hello_world',
+            charset="utf8mb4")
+        cursor = conn.cursor(dictionary=True)
+        sql = "SELECT * FROM hello_world.hello_world;"        
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        cursor.close()
+        df= pd.DataFrame(res)
+        return df
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    finally:
+        if (conn.is_connected()):
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")      
+            
