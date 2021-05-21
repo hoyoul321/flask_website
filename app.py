34ff2849b66a81e68200cbadbd9707a5863df211
hoyoul_lee <hoyoul321@gmail.com>
2021-05-21 16:07:48 +0900

    -외부 사용

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def index():
     from utils import mysql
     print("here")
     res =  mysql.mysql_sample_query()
-    print(res)
+    #print(res)
     return render_template('index.html', data =res )
 
 if __name__=="__main__":
-    app.run(debug=True, host = '0.0.0.0'  , port = 5000) 
+    app.run(debug=True, host = '0.0.0.0')
 
