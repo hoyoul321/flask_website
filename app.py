3a246d4e32aaedd47097f7a508a50b6534dfc840
hoyoul_lee <hoyoul321@gmail.com>
2021-05-21 18:18:41 +0900

    --flask app.py 외부 접속환경

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask, Blueprint, request, render_template, flash, redirect, url_for
 #Flask 객체 인스턴스 생성
-main = Blueprint( 'main', __name__, url_prefix='/')
+
 app = Flask(__name__)
+
 
 @app.route('/', methods= ['GET']) # 접속하는 url
 def index():
@@ -13,5 +14,5 @@ def index():
     return render_template('index.html', data =res )
 
 if __name__=="__main__":
-    app.run(debug=True, host = '0.0.0.0', port = 80)
+    app.run( host = '0.0.0.0') #debug=True,
 
